45e80ac9d330eb66464fd52de14934a1aeae7ca9
JacTBB <jackiesoon66@gmail.com>
2023-03-07 12:28:52 +0800

    Permissions Whitelist

--- index.js
+++ index.js
@@ -8,8 +8,17 @@ app.listen(port, () => {
     console.log(`Rest API listening on port ${port}`)
 })
 app.set('trust proxy', true)
+const noblox = require('noblox.js')
 
 const DiscordURL = "https://discord.com"
+
+const Whitelist = [
+    ['User', 112469017], //JacTBB
+    ['Group', 6145535], //BP
+    ['User', 669150884], //Arioch
+    ['Group', 9032731], //Seward
+    ['Group', 5431795], //SCPF
+]
 
 //Rate Limiters
 limiter = rateLimit({
@@ -25,6 +34,36 @@ limiter = rateLimit({
 })
 
 //HTTP
+app.use(async (req, res, next) => {
+    const PlaceID = req.headers['roblox-id']
+
+    fetch(`https://apis.roblox.com/universes/v1/places/${PlaceID}/universe`)
+    .then(response => response.json())
+    .then(async (data) => {
+        const UniverseID = data['universeId']
+
+        try {
+            const universeInfo = await noblox.getUniverseInfo([ UniverseID ])
+            const Type = universeInfo[0]['creator']['type']
+            const CreatorID = universeInfo[0]['creator']['id']
+            
+            for (i of Whitelist) {
+                console.log(Type,CreatorID)
+                if (i[0] == Type && i[1] == CreatorID) {
+                    return next()
+                }
+            }
+
+            return res.status(401).end()
+        }
+        catch (error) {
+            console.log(error)
+            return res.status(401).end()
+        }
+    })
+    .catch(console.error)
+})
+
 app.get('/', limiter, async (req, res) => {
     res.status(200).json({status: 'Online!'})
 })
